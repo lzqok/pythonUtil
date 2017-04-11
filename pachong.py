60d01c9c67dd50f6058ca033268cd3ae63c7d014
lzqok <lzq511236658@qq.com>
2017-04-11 17:38:51 +0800

    fix

--- pachong.py
+++ pachong.py
@@ -25,37 +25,49 @@
 from splinter.browser import Browser
 from time import sleep
 import traceback
-username = u"13557847327"
-password = u"123654zq"
+username = u"你的用户名"
+password = u"你的密码"
 home_url = "http://www.228.com.cn"
 login_url = "http://www.228.com.cn/auth/login"
 zhangxueyou_url = "http://www.228.com.cn/ticket-238802251.html"
 oreder_url = "http://www.228.com.cn/cart/toOrderSure.html"
-
+pay_oreder = "https://excashier.alipay.com/standard/auth.html"
+ticket_num = "ticket"
 def login():
 	sleep(1)
 	b.fill("username",username)
 	sleep(1)
 	b.fill("password",password)
 	b.find_by_id(u"loginsubmit").click()
-	sleep(3)
 def qianpiao():
 	global b
 	b = Browser(driver_name="chrome")
 	b.visit(login_url)
 	login()
-	# sleep(2)
 	b.visit(zhangxueyou_url)
-	# if is_text_present(zhangxueyou_url):
+
+	# if is_text_presenßßt(zhangxueyou_url):
 	ul = b.find_by_name(u"2017[A CLASSIC TOUR学友.经典] 世界巡回演唱会—扬州站")
-	li = ul[2].check()
-	if not li[2].is_selected():
-		li[3].check()
+	index = 1
+	print(len(ul))
+	for i in range(index,len(ul)):
+		ul[i].check()
+		if ul[i]['class'] != 'on':
+			sleep(0.5)
+			b.find_by_xpath(u'//*[@id="JqueBoxer"]/div[1]/a').click()
+		else:
+			break
+	if login_url in b.url:
+	 	login()
+
+	b.find_by_xpath(u'//*[@id="JreltList"]/ul/li[3]/dl/input').fill(ticket_num)
+
 	b.find_by_xpath(u"/html/body/div[12]/div[2]/div[3]/div[5]/a").click()
 
 	b.find_by_xpath(u"//*[@id='zidTabs']/li[2]").check()
-	sleep(2)
+
 	b.find_by_xpath(u'//*[@id="payNew-conts-show"]/div[2]/ul/li[2]/input').click()
-	# b.find_by_id(u'saveOrder').click()
+	while pay_oreder not in b.url:
+		b.find_by_id(u'saveOrder').click()
 if __name__ == "__main__":
 	qianpiao()
